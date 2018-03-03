58809d85a7d59d01db5158a4a0094576b533147a
Scott Barber <scott.barber@youearnedit.com>
2018-03-03 09:11:46 -0600

    Update it all for 2018

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 ## Synopsis
 
-A simple ruby script to parse / scrape SXSW 2016's showcase website. Also will provide json static file updates that you can use in your scripts (ie: `https://raw.githubusercontent.com/sbarber/sxsw_scraper/master/sxsw-showcases-20160224134325.json`)
+A simple ruby script to parse / scrape SXSW 2018's showcase website. Also will provide json static file updates that you can use in your scripts (ie: `https://raw.githubusercontent.com/sbarber/sxsw_scraper/master/sxsw-showcases-20180803090846.json`)
 
 ## Motivation
 
-wth? SXSW doesn't have a json output of their showcase data? I wrote a scraper to liberate the data!
+wth? SXSW doesn't have a json output of their showcase data? I wrote a scraper to expose the data!
 
 ## Installation
 
-Use with ruby > 2.2 -  `bundle install` and go!
+Use with ruby > 2.4 -  `bundle install` and go!
 
 ## Tests
 
--- sxsw_scraper.rb
+++ sxsw_scraper.rb
@@ -3,45 +3,31 @@
 require 'json'
 require 'nokogiri'
 require 'open-uri'
-
 require 'pp'
-
-BASE_URL = "http://schedule.sxsw.com/?conference=music&lsort=name&day=ALL&event_type=Showcase"
-alpha_list = %w(1 a b c d e f g h i j k l m n o p q r s t u v w x y z)
+require 'uri'
 
 showcases = []
-require 'rubygems'
 
-alpha_list.each do |a|
+page = Nokogiri::HTML(open("https://schedule.sxsw.com/2018/events/type/showcase?days=all"))
+page.encoding = 'utf-8'
 
-  artists = []
-  genres = []
 
-  page = Nokogiri::HTML(open("#{BASE_URL}&a=#{a}"))
-  page.encoding = 'utf-8'
+page.xpath("//div[contains(@class,'row single-event presented-by-row')]").each do |row|
 
-  page.xpath("//div[contains(@class,'row eventcol')]").each do |row|
+  showcase_url = row.attributes["data-event-url"].value
+  artist = row.xpath('div/h4/a')[0].content
+  datetime = row.xpath('div[2]/div')[0].content.gsub('2018', '2018 ')
+  venue = row.xpath('div[3]/div')[0].children.first.content
+  genre = row.xpath('div[4]/div')[0].children.first.content.strip
 
-    # artist
-    artist = row.xpath('div/div[1]/div/div/div/a')[0].content
-    
-    # genre
-    genre = row.xpath('div/div[2]/div/div/div')[0].content.gsub('Showcase','').strip!
-
-    # venue
-    venue = row.xpath('div/div[2]/div/div[2]')[0].content.strip!
-
-    # date
-    date = row.xpath('div/div[2]/div/div[3]')[0].content.strip.gsub(/\s+/, " ")
-
-    showcases << { artist: artist,
-                   genre: genre,
-                   venue: venue,
-                   date: date
-                 }
-  end
+  showcases << {
+    artist:       artist,
+    datetime:     datetime,
+    venue:        venue,
+    genre:        genre,
+    showcase_url: showcase_url
+  }
 
 end
 
 puts JSON.pretty_generate(showcases)
-#pp showcases
